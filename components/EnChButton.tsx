9ec77217cd1e8b27fb3d13191e8513353ee69c22
nilswg <nilsonweng@gmail.com>
2023-02-17 14:32:22 +0800

    [Update] i18n 多語系，更新語系切換按鈕

--- components/EnChButton.tsx
+++ components/EnChButton.tsx
@@ -1,23 +1,38 @@
-import { useStores } from '@/stores'
+import { useRouter } from 'next/router'
 
 type LangInputProps = {
   className: string
 }
 
 const LangInput = ({ className }: LangInputProps) => {
-  const lang = useStores((state) => state.lang)
-  const switchLang = useStores((state) => state.switchLang)
+  const router = useRouter()
+
+  /**
+   * 語系切換:
+   * (1) en -> zh-TW
+   * (2) zh-TW -> en
+   */
+  const handleLocaleChange = (event: any) => {
+    const value = router.locale === 'zh-TW' ? 'en' : 'zh-TW'
+    router.push(router.route, router.asPath, {
+      locale: value,
+    })
+  }
 
   return (
     <input
       type="checkbox"
       className={`
         box-content inline-block h-4 w-8 cursor-pointer appearance-none border border-white before:absolute before:h-4 before:w-4 before:bg-white before:transition-transform before:duration-300 before:ease-in-out before:content-['']
-        ${lang === 'en' ? 'before:translate-x-0' : 'before:translate-x-[100%]'}
+        ${
+          router.locale === 'en'
+            ? 'before:translate-x-0'
+            : 'before:translate-x-[100%]'
+        }
         ${className}
       `}
-      onChange={switchLang}
-      checked={lang === 'en' ? true : false}
+      onChange={handleLocaleChange}
+      checked={router.locale === 'en' ? true : false}
     />
   )
 }
@@ -28,13 +43,14 @@ type Props = {
 }
 
 const EnChButton = ({ className, inputProps }: Props) => {
+  const { locale } = useRouter()
   return (
     <label
       className={`flex cursor-pointer items-center gap-1 py-2 px-2 font-russon ${className}`}
     >
-      <span>EN</span>
+      <span>{locale === 'en' ? 'EN' : '英'}</span>
       <LangInput {...inputProps} />
-      <span>CH</span>
+      <span>{locale === 'en' ? 'CH' : '中'}</span>
     </label>
   )
 }
