5bd9cbef450d60bef4addac8523c0b936c758aa1
nilswg <nilsonweng@gmail.com>
2023-02-20 00:52:12 +0800

    [Update] 移除無用的store屬性: lang

--- stores/index.ts
+++ stores/index.ts
@@ -1,25 +1,13 @@
 import { create } from 'zustand'
 
-type langType = 'en' | 'ch'
-
-type LangStore = {
-  lang: langType
-  switchLang: () => void
-}
-
 type NavbarStore = {
   isMenuOpen: boolean
   switchMenuOpen: () => void
 }
 
-type Store = LangStore & NavbarStore
+type Store = NavbarStore
 
 export const useStores = create<Store>((set) => ({
-  lang: 'en',
-  switchLang: () => {
-    set((state) => ({ lang: state.lang === 'en' ? 'ch' : 'en' }))
-  },
-
   isMenuOpen: false,
   switchMenuOpen: () => {
     set((state) => ({ isMenuOpen: !state.isMenuOpen }))
