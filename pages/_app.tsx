b7f716721c51db2e0fbf33661a968d9ceb678949
nilswg <nilsonweng@example.com>
2024-04-04 23:42:13 +0800

    [update] 移除 sass，簡化樣式開發流程

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout'
 import { appWithTranslation } from 'next-i18next'
 import type { AppProps } from 'next/app'
-import 'styles/globals.css'
+import './globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
