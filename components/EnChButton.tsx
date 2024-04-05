37c05c086466342ecb844255ba6bb60f7ad7690b
nilswg <nilsonweng@example.com>
2024-04-05 16:05:57 +0800

    [update] EnchButton 多語系切換方式，不用 next/router

--- components/EnChButton.tsx
+++ components/EnChButton.tsx
@@ -1,109 +1,87 @@
+'use client'
+
 import cookie from 'js-cookie'
-import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
-type LangInputProps = {
-  className: string
+import { useI18n } from './useI18n'
+import { type FC, useCallback, ReactNode } from 'react'
+
+type EnChCompoundComponent = FC<{ className: string; children: ReactNode }> & {
+  CheckBox: FC<{ className: string }>
 }
 
-const LangInput = ({ className }: LangInputProps) => {
-  const router = useRouter()
+const EnChButton: EnChCompoundComponent = ({ className, children }) => {
+  const { t } = useI18n('common')
+  return (
+    <label className={`flex cursor-pointer items-center gap-1 px-2 py-2 font-russon ${className}`}>
+      <span>{t('nav.en', { defaultValue: 'EN' })}</span>
+      {children}
+      <span>{t('nav.ch', { defaultValue: 'CH' })}</span>
+    </label>
+  )
+}
 
-  /**
-   * 語系切換:
-   * (1) en -> zh-TW
-   * (2) zh-TW -> en
-   */
-  const handleLocaleChange = (event: any) => {
-    const value = router.locale === 'zh-TW' ? 'en' : 'zh-TW'
-    router.push(router.route, router.asPath, {
-      locale: value,
-    })
-
-    /**
-     * 當使用者自行切換使用的語言時，將結果保存至 cookies 中。
-     * 便於 i18n 下次瀏覽網站時，會自動跳轉到對應語言的網頁。
-     *
-     * 請參閱: https://nextjs.org/docs/advanced-features/i18n-routing#leveraging-the-next_locale-cookie
-     *
-     * 但是，如果在 next.config.js 中，設置 localeDetection = false 時，
-     * 將會導致此功能被關閉。
-     *
-     * 請參閱: https://nextjs.org/docs/advanced-features/i18n-routing#disabling-automatic-locale-detection
-     */
-
-    cookie.set('NEXT_LOCALE', value, { expires: 365 })
-  }
+EnChButton.CheckBox = ({ className }: LangInputProps) => {
+  const { i18n } = useI18n()
+  const handleLocaleChange = useCallback(() => {
+    const newLang = i18n.language === 'en' ? 'zh-TW' : 'en'
+    switchLanguage(newLang)
+    saveLocaleToCookie(newLang)
+  }, [])
 
   return (
     <input
       type="checkbox"
       className={`
-        box-content inline-block h-4 w-8 cursor-pointer appearance-none border border-white before:absolute before:h-4 before:w-4 before:bg-white before:transition-transform before:duration-300 before:ease-in-out before:content-['']
-        ${
-          router.locale === 'en'
-            ? 'before:translate-x-0'
-            : 'before:translate-x-[100%]'
-        }
+        box-content inline-block h-4 w-8 cursor-pointer appearance-none border border-white before:absolute before:h-4 before:w-4 before:bg-white before:transition-transform before:duration-300 before:ease-in-out before:content-[''] group-hover:border-sky-500 before:group-hover:bg-sky-500
+        ${i18n.language === 'en' ? 'before:translate-x-0' : 'before:translate-x-[100%]'}
         ${className}
       `}
       onChange={handleLocaleChange}
-      checked={router.locale === 'en' ? true : false}
+      checked={i18n.language === 'en' ? true : false}
     />
   )
 }
 
-type Props = {
-  className: string
-  inputProps: LangInputProps
-}
-
-const EnChButton = ({ className, inputProps }: Props) => {
-  const { t } = useTranslation('common')
-  return (
-    <label
-      className={`flex cursor-pointer items-center gap-1 py-2 px-2 font-russon ${className}`}
-    >
-      <span>{t('nav.en', { defaultValue: 'EN' })}</span>
-      <LangInput {...inputProps} />
-      <span>{t('nav.ch', { defaultValue: 'CH' })}</span>
-    </label>
-  )
-}
 export default EnChButton
 
-// Example
-//
-// const Example = () => (
-//   <EnChButton
-//     className="text-3xl"
-//     inputProps={{
-//       className: 'h-[30px] w-[60px] before:h-[30px] before:w-[30px]',
-//     }}
-//   />
-// )
+type LangInputProps = {
+  className: string
+}
 
-// export const MyTest1 = ({ className, inputProps }: Props) => {
-//   console.log('MyTest1')
-//   return (
-//     <label
-//       className={`flex cursor-pointer items-center gap-1 py-2 px-2 font-russon ${className}`}
-//     >
-//       <span>EN</span>
-//       <LangInput {...inputProps} />
-//       <span>CH</span>
-//     </label>
-//   )
-// }
+function switchLanguage(language: string) {
+  // 在這個函數中，你可以根據所選的語言生成新的URL
+  var currentPath = window.location.pathname // 取得目前的路徑
+  var newPath = ''
 
-// export const MyTest2 = ({ className, inputProps }: Props) => {
-//   console.log('MyTest2')
-//   return (
-//     <label
-//       className={`flex cursor-pointer items-center gap-1 py-2 px-2 font-russon ${className}`}
-//     >
-//       <span>EN</span>
-//       {LangInput({ ...inputProps })}
-//       <span>CH</span>
-//     </label>
-//   )
-// }
+  // 根據所選的語言來修改路徑
+  switch (language) {
+    case 'en':
+      newPath = currentPath.replace(/\/zh-TW\/?/, '/en/') // 將 'zh-TW' 替換為 'en'
+      break
+    case 'zh-TW':
+      newPath = currentPath.split('/')[1] === 'en' ? currentPath.replace(/\/en\/?/, '/zh-TW/') : '/zh-TW' + currentPath
+      break
+  }
+
+  // 將網頁重新導向到新的URL
+  // window.location.href = window.location.origin + newPath
+  // use pushState
+  window.history.pushState({}, '', newPath) //
+
+  // go to new page
+  window.location.reload()
+}
+
+function saveLocaleToCookie(locale: string) {
+  /**
+   * 當使用者自行切換使用的語言時，將結果保存至 cookies 中。
+   * 便於 i18n 下次瀏覽網站時，會自動跳轉到對應語言的網頁。
+   *
+   * 請參閱: https://nextjs.org/docs/advanced-features/i18n-routing#leveraging-the-next_locale-cookie
+   *
+   * 但是，如果在 next.config.js 中，設置 localeDetection = false 時，
+   * 將會導致此功能被關閉。
+   *
+   * 請參閱: https://nextjs.org/docs/advanced-features/i18n-routing#disabling-automatic-locale-detection
+   */
+  cookie.set('NEXT_LOCALE', locale, { expires: 365 })
+}
